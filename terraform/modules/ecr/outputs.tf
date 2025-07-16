3e56f768bce6e0b612a224e7692caef814f8b135
Guilherme Oliveira <oliveira.g.ar@proton.me>
2025-07-16 11:41:40 -0300

    fix ecr terraform module output

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_uri" {
   description = "The ECR repo URI"
-  value = aws_ecr_repository.ecr_repo.repository_url
+  value = [for ecr in aws_ecr_repository.ecr_repo.repository_url : ecr.id]
 }
